d2460c24fbce2a50c72c878095dacc94f169ca19
jochchristopher <jochchristopher@gmail.com>
2022-09-04 17:26:11 +0200

    added npm skripts, omptimizer for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 import * as path from 'path';
+
+import TerserPlugin from "terser-webpack-plugin";
+
+const PROD = JSON.parse(process.env.PROD_ENV || "0");
+
 const config = {
     entry: './src/index.ts',
     output: {
@@ -23,7 +28,14 @@ const config = {
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    watchOptions: {
+        ignored: /node_modules|dist/,
+    },
+    optimization: {
+        minimize: !!PROD,
+        minimizer: [new TerserPlugin()],
+    },
 };
 
 export default config;
